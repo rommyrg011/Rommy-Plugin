1a71ee54eaa94ac1fe0a95317b0055d0ba1430ff
Rommy Gunawan <rommmygunawan66@gmail.com>
2024-03-05 14:53:16 +0800

    -

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin URI: http://tesaja.liveblog365.com
 Description: Ini hanya latihan plugin wordpress pertamaku
 Version: 1.0.0
 Author: Rommy Gunawan
-Author URI: https://tesaja.liveblog365.com
+Author URI: http://tesaja.liveblog365.com
 */
 
 define('TEMP_DIR', plugin_dir_path(__FILE__) . '/templates/');
